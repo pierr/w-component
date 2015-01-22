e07177f173d8bb2b83402367008de1ac8e40b520
unknown <pbesson@pbesson-port.KLEE.LAN.NET>
2015-01-22 23:17:39 +0100

    [build] Add serve task.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+// Watch Files For Changes & Reload
+gulp.task('serve', function () {
+  browserSync({
+    notify: true,
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.']
+    }
+  });
